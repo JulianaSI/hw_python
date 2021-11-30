f19d51d2d074839913656eb64d5b32f3b3921e03
JulianaSI <94391329+JulianaSI@users.noreply.github.com>
2021-11-30 20:22:54 +0500

    Delete hw_24.11.21 directory

--- hw_24.11.21/hw1.py
+++
@@ -1,9 +0,0 @@
-full_name = input('Введите ФИО менеджера: ')
-zakaz = int(input('Введите количество обработанных заказов: '))
-summa_zak = float(input('Введите общую стоимость заказов: '))
-result = summa_zak / zakaz
-result = f'Средняя стоимость одного заказа {result:.2f} руб.'
-print(full_name)
-print(zakaz, ' (количество заказов)')
-print(summa_zak, ' руб. (общая стоимость заказов)')
-print(result)
